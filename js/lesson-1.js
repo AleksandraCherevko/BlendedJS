62f13753f3186f1ed71bf6e2133f1fa3affeb717
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-11-30 12:26:13 +0100

    Update lesson-1.js

--- js/lesson-1.js
+++ js/lesson-1.js
@@ -221,3 +221,62 @@
 // }
 
 // fizzBuzz(20);
+
+// Завдання 11
+
+// Оголоси функцію makeTransaction, яка очікує два параметри, значення яких будуть
+// задаватися під час її виклику: • quantity— перший параметр, число, що містить кількість
+// замовлених дроїдів • pricePerDroid — другий параметр, число, що містить вартість одного дроїда
+// Доповни код функції так, щоб вона повертала рядок з повідомленням про покупку ремонтних дроїдів:
+// "You ordered <quantity> droids worth <totalPrice> credits!", де:
+// • <quantity> — це кількість замовлених дроїдів
+// • <totalPrice> — це загальна вартість замовлення, тобто вартість усіх замовлених дроїдів
+
+// Візьми код нижче і встав після оголошення своєї функції для перевірки коректності її роботи.
+// У консоль будуть виведені результати її роботи.
+
+// function makeTransaction(quantity, pricePerDroid) {
+//   const totalPrice = quantity * pricePerDroid;
+//   return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+// }
+
+// console.log(makeTransaction(5, 3000));
+
+// Завдання 12
+
+// Оголоси функцію getShippingMessage, яка очікує три параметри,
+// значення яких будуть задаватися під час її виклику:
+// • country — перший параметр, рядок, що містить країну доставки
+// • price — другий параметр, число, що містить загальну вартість товару
+// • deliveryFee — третій параметр, число, що містить вартість доставки товару
+
+// Доповни код функції так, щоб вона повертала рядок з повідомленням про доставку товару в країну користувача:
+// "Shipping to <country> will cost <totalPrice> credits", де:
+// • <country> — це країни доставки
+// • <totalPrice> — це загальна вартість замовлення, що включає вартість товару і його доставки
+
+// function getShippingMessage(country, price, deliveryFee) {
+//   const totalPrice = price + deliveryFee;
+//   return `Shipping to ${country} will cost ${totalPrice} credits`;
+// }
+
+// console.log(getShippingMessage("Australia", 120, 50));
+
+// Завдання 13
+// Оголоси функцію getElementWidth, яка очікує три параметри,
+// значення яких будуть задаватися під час її виклику:
+// • content— перший параметр, ширина контенту
+// • padding — другий параметр, значення горизонтального падінгу для кожної зі сторін
+// • border — третій параметр, значення товщини бордера для кожної зі сторін
+// Значення всіх параметрів будуть рядками формату Npx де N — це довільне число, ціле або дробове.
+
+// Доповни код функції так, щоб вона повертала число —загальну ширину елемента.
+// При розрахунку загальної ширини орієнтуйся на те, що значення box-sizing дорівнює border-box.
+
+// function getElementWidth(content, padding, border) {
+//   return parseFloat(content) + 2 * parseFloat(padding) + 2 * parseFloat(border);
+// }
+// console.log(getElementWidth("50px", "8px", "4px")); // 74
+// console.log(getElementWidth("60px", "12px", "8.5px")); //101
+
+// Завдання 14
