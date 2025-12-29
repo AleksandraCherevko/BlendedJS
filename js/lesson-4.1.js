4dd8e422349a1b385829d500b20d206271b05da4
AleksandraCherevko <119119431+AleksandraCherevko@users.noreply.github.com>
2025-12-29 22:57:44 +0100

    add new task

--- js/lesson-4.1.js
+++ js/lesson-4.1.js
@@ -70,3 +70,17 @@ const markup = images
   .join("");
 
 imgGallery.insertAdjacentHTML("beforeend", markup);
+
+// Напиши скрипт, який під час набору тексту в інпуті input#name-input
+// (подія input) підставляє його поточне значення в span#name-output
+// як ім’я для привітання. Обов’язково очищай значення в інпуті
+// по краях від пробілів . Якщо інпут порожній або містить лише пробіли,
+// то замість імені у спан має підставлятися рядок "Anonymous".
+
+const input = document.querySelector("#name-input");
+const output = document.querySelector("#name-output");
+
+input.addEventListener("input", (event) => {
+  const inputValue = event.currentTarget.value.trim();
+  output.textContent = inputValue === "" ? "Anonymous" : inputValue;
+});
